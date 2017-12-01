ac084ee4e8f6c5bb9fc3192715b0d96e97c00499
chamaine <chamaine@wellington-studio.com>
2017-11-30 20:37:04 -0800

    index and testimonial pages

---
+++ public/assets/js/carousel.js
@@ -0,0 +1,19 @@
+/*
+ * Handle all the Javascript logic for the carousel
+ */
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.js"></script>
+ 
+$(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:2,
+        itemsDesktop:[1000,2],
+        itemsDesktopSmall:[990,1],
+        itemsTablet:[768,1],
+        pagination:true,
+        navigation:false,
+        navigationText:["",""],
+        slideSpeed:1000,
+        autoPlay:true
+    });
+});
