5a2f96d2516b964fa438c1183797ee5642a5f9b1
chamaine <chamaine@wellington-studio.com>
2017-12-03 12:31:06 -0800

    FrontEnd

--- public/assets/js/carousel.js
+++ public/assets/js/carousel.js
@@ -1,8 +1,8 @@
 /*
- * Handle all the Javascript logic for the carousel
+ * Handle all the Javascript logic for the carousel on testimonial page
  */
-<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.js"></script>
+// <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+// <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.js"></script>
  
 $(document).ready(function(){
     $("#testimonial-slider").owlCarousel({
