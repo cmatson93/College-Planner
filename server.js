d42b5465fdb267bc198b7584169d44a4af4b1b9f
Kumari Priya <priya@goel.pw>
2017-12-03 13:25:06 -0800

    passport changes

--- server.js
+++ server.js
@@ -6,12 +6,23 @@ var exphbs = require("express-handlebars");
 var db = require('./models');
 var request = require('request');
 
+var passport   = require('passport')
+var session    = require('express-session')
+var bodyParser = require('body-parser')
+var env        = require('dotenv').load()
+
+
 const PORT = process.env.PORT || 3000;  // Port to listen to
 
 var app = express();  // Get express handle
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// For Passport
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
 
 // Set Handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
@@ -21,7 +32,13 @@ app.set("view engine", "handlebars");
 // var routes = require("./routes/college.js");
 // app.use("/", routes);
 
-require("./routes/college.js")(app);
+require("./routes/college.js")(app,passport);
+
+
+//load passport strategies
+require('./config/passport/passport.js')(passport,db.User);
+
+
 // require("./routes/crud.js")(app);
 
 //Start listening when connection to server is successful
@@ -31,5 +48,3 @@ db.sequelize.sync().then(function() {
 		console.log("Listening on port: " + PORT);
 	});
 });
-
-
