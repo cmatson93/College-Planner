53422cfcc8c39aebe285b7e84197c8a353bb244b
Kumari Priya <priya@goel.pw>
2017-12-03 14:31:38 -0800

    remove unnecessary findone, it's already done by passport

--- routes/college.js
+++ routes/college.js
@@ -100,17 +100,7 @@ module.exports =  function(app, passport){
 	// Post from the Sign In or Registrtion Form
 	app.get("/profile",isLoggedIn, function(req, res) {
 		console.log("Post User Sig In Data");
-		// console.log(req.user.id);
-			db.User.findOne(
-			{
-			    where: {
-			      id: req.user.id
-			    }
-			})
-			.then(function(result) {
-				console.log(result.dataValues);
-			    if (result != null){
-			    	// Save the current user in the global variable
+
 			    	userId = req.user.id;
 						userObj = {
 						id: req.user.id,
@@ -123,8 +113,7 @@ module.exports =  function(app, passport){
 				  		todoList: []
 					};
 						renderUserCollege(userObj, res);
-				}
-					});
+			
 
 	});
 
